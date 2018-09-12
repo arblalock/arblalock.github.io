b0af2bd6a690684967e2f4c412c2ab4478a714e9
arblalock <aadamrblalock@gmail.com>
2018-09-11 20:09:26 -0700

    added overlay support for mobile

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-  $("[data-toggle=popover]").popover({
-    html: true
-  });
+  $("[data-toggle=popover]").popover({html: true});
   $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'})
 
   $("#link-row").click(function() {
@@ -21,7 +19,8 @@ $(document).ready(function() {
     }
   });
 
-  $(document).on('click', '#email-copy', function() {
+  $(document).on('click', '#email-copy', function(e) {
+    e.preventDefault();
     var $temp = $("<input>");
     $("body").append($temp);
     $temp.val("adamrblalock@gmail.com").select();
@@ -34,9 +33,48 @@ $(document).ready(function() {
 
   $(document).on('click', '#email_to_link', function() {
     var email = 'adamrblalock@gmail.com';
-    window.location = 'mailto:' + email;
+    location.href = 'mailto:' + email;
   })
 
+  function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-});
-;
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+      return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+      return "Android";
+    }
+
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return "iOS";
+    }
+
+    return "unknown";
+  }
+
+  if(getMobileOperatingSystem() != "unknown"){
+    hideOverlays();
+    $('.btn-img').on('click', function() {
+      $(this).find('.link-hover-cont').css('display', 'flex');
+      $(this).find('img').css({'opacity': '0.2'});
+    })
+    $('.site-cont').on('click', function(e) {
+      let container = $(".btn-img");
+      // if the target of the click isn't the container nor a descendant of the container
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        hideOverlays();
+      }
+    })
+}
+
+  function hideOverlays() {
+    $('.link-hover-cont').css({'display': 'none', 'opacity': '1'});
+    $('.btn-img img').css({'opacity': '1'});
+    $('.img-wrap').css('background-color', 'black');
+    overlayOpen = false;
+  }
+
+}); // end on doc ready
